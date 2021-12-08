09a6e153696cc8bb3ea28e3bf61988697c8cb7aa
raaaahman <sylvain@devindetails.com>
2021-12-08 09:18:22 +0100

    feat: Exemple récupération forumlaire Magiquiz

--- magiquiz/main.js
+++ magiquiz/main.js
@@ -17,6 +17,7 @@ const answers = {
 
 document.getElementById('question').innerText = questions[0].text
 
+// Remplir les valeurs du formulaire
 const choices = document.getElementsByClassName('answer-choice')
 choices[0].setAttribute('value', 'tigruby')
 document.querySelector('[for="answer-0"]').innerText = questions[0].tigruby
@@ -29,3 +30,24 @@ document.querySelector('[for="answer-2"]').innerText = questions[0].becDeCorbin
 
 choices[3].setAttribute('value', 'ratonifiant')
 document.querySelector('[for="answer-3"]').innerText = questions[0].ratonifiant
+
+// Récupérer le choix de l'utilisateur
+document.getElementById('answer').addEventListener(
+    'submit',
+    function(event) {
+        event.preventDefault()
+        
+        const choices = event.target.getElementsByClassName('answer-choice')
+
+        console.log(choices[0].value, choices[0].checked)
+        console.log(choices[1].value, choices[1].checked)
+        console.log(choices[2].value, choices[2].checked)
+        console.log(choices[3].value, choices[3].checked)
+
+        // Remettre à zéro
+        choices[0].checked = false
+        choices[1].checked = false
+        choices[2].checked = false
+        choices[3].checked = false
+    }
+)
